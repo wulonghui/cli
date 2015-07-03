02e22788d8813142939f7c06d6dfa8eaef36894e
wulonghui <wlh6666@qq.com>
2015-07-03 19:28:45 +0800

    Update example

--- example/main.go
+++ example/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"github.com/wulonghui/cli"
 	_ "github.com/wulonghui/cli/example/commands"
+	. "github.com/wulonghui/cli/i18n"
 )
 
 func main() {
 	cli := cli.New("example", "1.0.0")
-	cli.Usage = "This is a example"
+	cli.Usage = T("This is a example")
 	cli.Run()
 }
